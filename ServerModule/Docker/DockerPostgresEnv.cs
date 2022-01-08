6a5ed2710dd0208d09d7ee30f0946302490fbd2c
Administrator <admin@example.com>
2022-01-08 01:18:47 +0100

    added port env

--- ServerModule/Docker/DockerPostgresEnv.cs
+++ ServerModule/Docker/DockerPostgresEnv.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using DebugAndTrace;
+using ServerModule.Utility;
+using Char = System.Char;
 
 namespace ServerModule.Docker
 {
@@ -14,17 +17,29 @@ namespace ServerModule.Docker
         public static readonly string ContainerName;
         public static readonly string PostgresUser;
         public static readonly string PostgresPassword;
+        public static readonly string PostgresPort;
 
         static DockerPostgresEnv()
         {
+            // Change this, if using another container name
             ContainerName = "swe1db";
-            // Environment Variables of docker container
+            // Change this, if using other Environment Variables of docker container
             const string usernameEnv = "POSTGRES_USER";
             const string passwordEnv = "POSTGRES_PASSWORD";
             // ReSharper disable once StringLiteralTypo
             PostgresUser = ExecuteCommand($"docker exec {ContainerName} printenv {usernameEnv}");
             // ReSharper disable once StringLiteralTypo
             PostgresPassword = ExecuteCommand($"docker exec {ContainerName} printenv {passwordEnv}");
+            string rawPort = ExecuteCommand($"docker ps --filter \"name={ContainerName}\" --format \"{{{{.Ports}}}}\"");
+            // Example: 0.0.0.0:5432->5432/tcp 
+            int startIndex = rawPort.IndexOf(Utils.GetChar(Utility.Char.Colon)) + 1;
+            int endIndex = rawPort.IndexOf(Utils.GetChar(Utility.Char.Minus));
+            if (startIndex >= 0 && rawPort.Length > startIndex) PostgresPort = rawPort[startIndex..endIndex];
+            else
+            {
+                Printer.Instance.WriteLine("Port is invalid. Check if port is exposed. Using default port 5432 ...");
+                PostgresPort = "5432";
+            }
         }
 
         private static string ExecuteCommand(string command)
