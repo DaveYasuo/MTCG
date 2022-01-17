e10803c0da9a15e83cdd70c918af604b12477887
Administrator <admin@example.com>
2022-01-17 01:05:14 +0100

    added battle classes

---
+++ MTCG/BattleLogic/Battle.cs
@@ -0,0 +1,57 @@
+﻿using DebugAndTrace;
+using MTCG.Data.Cards;
+using MTCG.Data.Users;
+using MTCG.Data.Utils;
+using MTCG.Logging;
+
+namespace MTCG.BattleLogic
+{
+    class Battle
+    {
+        private readonly IPlayer _player1;
+        private readonly IPlayer _player2;
+        private readonly IBattleLog _battleLog;
+
+        public Battle(IPlayer player1, IPlayer player2, IBattleLog battleLog)
+        {
+            _player1 = player1;
+            _player2 = player2;
+            _battleLog = battleLog;
+        }
+
+        public void StartGame()
+        {
+            _battleLog.AddStartInfo();
+            for (int round = 1; round <= 100; round++)
+            {
+                ICard card1 = _player1.GetRandomCard();
+                ICard card2 = _player2.GetRandomCard();
+                (float damage1, float damage2) = Utility.CalculateDamage(card1, card2);
+                switch (damage1.CompareTo(damage2))
+                {
+                    case > 0:
+                        {
+                            _player1.Add(card2);
+                            _player2.Remove(card2);
+                            break;
+                        }
+                    case < 0:
+                        {
+                            _player2.Add(card1);
+                            _player1.Remove(card1);
+                            break;
+                        }
+                }
+                _battleLog.AddRound(damage1, damage2, round);
+                if (_player1.Cards.Count == 0 || _player2.Cards.Count == 0)
+                {
+                    break;
+                }
+            }
+        }
+        public BattleResult GetResult()
+        {
+            return  _battleLog.GetResult();;
+        }
+    }
+}
---
+++ MTCG/BattleLogic/BattleResult.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace MTCG.BattleLogic
+{
+    public class BattleResult
+    {
+        public bool Draw { get; }
+        public string Winner { get; }
+        public string Loser { get; }
+        public List<string> Log { get; }
+
+
+        public BattleResult(List<string> log)
+        {
+            Log = log;
+            Draw = true;
+        }
+
+        public BattleResult(string winner, string loser, List<string> log)
+        {
+            Draw = false;
+            Winner = winner;
+            Loser = loser;
+            Log = log;
+        }
+    }
+}
